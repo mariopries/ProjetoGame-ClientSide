a494dada7a8acb787c491f4d9a6df849a34c97d3
Lucas3995 <Lucas3995@hotmail.com>
2019-02-15 13:35:19 -0200

    primeiro script
    adicionado um script "vazio" no player e começado a testar uma mecanica basica de movimento em X e Y pelo Axis

---
+++ ProjetoGame/Assets/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float teste=0;
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        transform.position+= new Vector3(moveHorizontal * speed, moveVertical * speed,0);
+    }
+}
