8d1686c7c0f9cb34e2d52a5e69b624afdd08fc26
Douglas dos Santos <douglasdossantos22@gmail.com>
2019-02-24 21:40:09 -0300

    Esboço scripts "Construction"

---
+++ ProjetoGame/Assets/Scripts/ProgramTeste.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramTeste : MonoBehaviour
+{
+
+    Castle castle;
+    Tower tower;
+    BuilderSpawn bspawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        castle = new Castle(0, 0, 0, 500, 100, 25);
+        tower = new Tower(0, 0, 0, 200, 50, 10);
+        bspawn = new BuilderSpawn(0, 0, 100, 60, 25);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Teste A");
+
+            castle.ReceiveDamage(100);
+
+            Debug.Log("Vida Castle : "+castle.HP);
+            Debug.Log("Escudo Castle : " + castle.Shield);
+            Debug.Log("Vida destruido? : " + castle.IsDestruido);
+
+
+            tower.ReceiveDamage(100);
+
+            Debug.Log("Vida Castle : " + tower.HP);
+            Debug.Log("Escudo Castle : " + tower.Shield);
+            Debug.Log("Vida destruido? : " + tower.IsDestruido);
+
+
+
+
+        }
+        
+    }
+}
