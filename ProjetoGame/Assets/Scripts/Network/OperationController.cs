80b223194f786466526d1e3c35c7b5f586ed925b
Douglas dos Santos <douglasdossantos22@gmail.com>
2019-03-03 23:33:10 -0300

    - Adicionado o photon framework que fornece as classes de conexão de rede entre os players e o servidor beckend. - Testes de conexão.

---
+++ ProjetoGame/Assets/Scripts/Network/OperationController.cs
@@ -0,0 +1,34 @@
+﻿using ExitGames.Client.Photon;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperationController : PhotonPeer
+{
+    protected internal static Type PingImplementation = null;
+
+    private readonly Dictionary<byte, object> opParameters = new Dictionary<byte, object>(); // used in OpRaiseEvent() (avoids lots of new Dictionary() calls)
+
+
+    /// <summary>
+    /// Creates a Peer with specified connection protocol. You need to set the Listener before using the peer.
+    /// </summary>
+    /// <remarks>Each connection protocol has it's own default networking ports for Photon.</remarks>
+    /// <param name="protocolType">The preferred option is UDP.</param>
+    public OperationController(ConnectionProtocol protocolType) : base(protocolType)
+    {
+        // this does not require a Listener, so:
+        // make sure to set this.Listener before using a peer!
+    }
+
+    /// <summary>
+    /// Creates a Peer with specified connection protocol and a Listener for callbacks.
+    /// </summary>
+    public OperationController(IPhotonPeerListener listener, ConnectionProtocol protocolType) : this(protocolType)
+    {
+        this.Listener = listener;
+    }
+
+ 
+}
