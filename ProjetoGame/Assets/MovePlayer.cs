f0b1347eaf2b5d7f75c075868e054b4318e564ef
Lucas3995 <Lucas3995@hotmail.com>
2019-03-07 13:33:31 -0300

    criando uma cena em conformidade com o curso do rpg pra fazer a base
    pra ja ter algo

---
+++ ProjetoGame/Assets/MovePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float vel;
+    private Vector2 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vel=3;
+        dir= Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+        transform.Translate(dir*vel*Time.deltaTime);
+    }
+
+    void Inputs()
+    {
+        dir=Vector2.zero;
+        if(Input.GetKey(KeyCode.UpArrow)){
+            dir+=Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            dir+=Vector2.down;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            dir+=Vector2.left;
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            dir+=Vector2.right;
+        }
+    }
+}
