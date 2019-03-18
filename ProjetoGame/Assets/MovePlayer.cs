9a409a1862f7538cb634f71af0fa3e30c9b56bc2
Mario Pries Junior <mario.hcsc@hotmail.com>
2019-03-18 13:27:09 -0300

    Commit do meio dia rs

--- ProjetoGame/Assets/MovePlayer.cs
+++ ProjetoGame/Assets/MovePlayer.cs
@@ -5,30 +5,20 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    public bool Andando;
     private float vel;
     private Vector2 dir;
     public Animator anim;
     private UnityArmatureComponent armatureComponent;
-
-    public MovePlayer()
-    {
-        armatureComponent = GetComponent<UnityArmatureComponent>();
-    }
+    private int _moveDir;
+    private int _IdleDir;
 
     // Start is called before the first frame update
     void Start()
     {
         vel = 3;
         dir = Vector2.zero;
-
         armatureComponent = GetComponent<UnityArmatureComponent>();
-        armatureComponent.animation.Play("Idle_Frente");
-
-        //UnityFactory.factory.LoadDragonBonesData("Personagem/Personagem_0001_ske");
-        //UnityFactory.factory.LoadTextureAtlasData("Personagem/Personagem_0001_tex");
-
-        //var armatureComponent = UnityFactory.factory.BuildArmatureComponent("Personagem");
-
         armatureComponent.animation.Play("Idle_Frente");
         Debug.Log(armatureComponent);
     }
@@ -38,51 +28,105 @@ public class MovePlayer : MonoBehaviour
     {
         Inputs();
         transform.Translate(dir * vel * Time.deltaTime);
-        AtualizaAnimation();
     }
 
     void Inputs()
     {
         dir = Vector2.zero;
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
             dir += Vector2.up;
+            this.Move(1);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             dir += Vector2.down;
+            this.Move(2);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             dir += Vector2.left;
+            this.Move(3);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             dir += Vector2.right;
+            this.Move(4);
+        }
+
+        else
+        {
+            this.Move(5);
+        }
+
+    }
+
+    public void Move(int direcao)
+    {
+        if (_moveDir == direcao)
+        {
+            return;
+        }
+        switch (direcao)
+        {
+            case 5:
+                this._IdleDir = _moveDir;
+                this.ChangeAnimationIdle();
+                break;
+            default:
+                this._moveDir = direcao;
+                this.ChangeAnimation();
+                break;
+        }
+        
+    }
+
+    public void ChangeAnimationIdle()
+    {
+        if (_IdleDir == 1)
+        {
+            armatureComponent.armature.flipX = false;
+            armatureComponent.animation.FadeIn("Idle_Frente");
+        }
+        if (_IdleDir == 2)
+        {
+            armatureComponent.armature.flipX = false;
+            armatureComponent.animation.FadeIn("Idle_Frente");
+        }
+        if (_IdleDir == 3)
+        {
+            armatureComponent.armature.flipX = true;
+            armatureComponent.animation.FadeIn("Idle_Lado");
+        }
+        if (_IdleDir == 4)
+        {
+            armatureComponent.armature.flipX = false;
+            armatureComponent.animation.FadeIn("Idle_Lado");
         }
     }
 
-    void AtualizaAnimation()
+    public void ChangeAnimation()
     {
-        if (Input.GetKey(KeyCode.UpArrow) && armatureComponent.animation.lastAnimationName != "Andando_Costas")
+        if (_moveDir == 1)
         {
             armatureComponent.armature.flipX = false;
-            armatureComponent.animation.Play("Andando_Costas");
+            armatureComponent.animation.FadeIn("Andando_Costas");
         }
-        if (Input.GetKey(KeyCode.DownArrow) && armatureComponent.animation.lastAnimationName != "Andando_Frente")
+        if (_moveDir == 2)
         {
             armatureComponent.armature.flipX = false;
-            armatureComponent.animation.Play("Andando_Frente");
+            armatureComponent.animation.FadeIn("Andando_Frente");
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && !armatureComponent.armature.flipX)
+        if (_moveDir == 3)
         {
             armatureComponent.armature.flipX = true;
-            armatureComponent.animation.Play("Andando_Direita");
+            armatureComponent.animation.FadeIn("Andando_Direita");
         }
-        if (Input.GetKey(KeyCode.RightArrow) && armatureComponent.armature.flipX)
+        if (_moveDir == 4)
         {
             armatureComponent.armature.flipX = false;
-            armatureComponent.animation.Play("Andando_Direita");           
+            armatureComponent.animation.FadeIn("Andando_Direita");
         }
     }
 }
